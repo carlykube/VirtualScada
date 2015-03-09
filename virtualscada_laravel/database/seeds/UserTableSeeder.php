c4d8cd0becec81e351508387466dde789dbedf28
kolengue <kolengue@icloud.com>
2015-03-09 15:57:33 -0500

    seeidng is done. type in composer dump-autoload; php artisan db:seed

--- virtualscada_laravel/database/seeds/UserTableSeeder.php
+++ virtualscada_laravel/database/seeds/UserTableSeeder.php
@@ -1,15 +1,38 @@
 <?php
 
+use Illuminate\Database\Seeder;
+
 class UserTableSeeder extends Seeder
 {
 
 public function run()
 {
     DB::table('users')->delete();
-    User::create(array(
+    App\User::create(array(
         'name'     => 'Chris Sevilleja',
         'username' => 'sevilayha',
         'email'    => 'chris@scotch.io',
+        'password' => Hash::make('awesome'),
+    ));
+
+    App\User::create(array(
+        'name'     => 'George Clinton',
+        'username' => 'george',
+        'email'    => 'george@smu.edu',
+        'password' => Hash::make('awesome'),
+    ));
+
+App\User::create(array(
+        'name'     => 'Bill Nooner',
+        'username' => 'bill',
+        'email'    => 'bill@scotch.io',
+        'password' => Hash::make('awesome'),
+    ));
+
+App\User::create(array(
+        'name'     => 'Ambers Selleja',
+        'username' => 'amber',
+        'email'    => 'amber@scotch.io',
         'password' => Hash::make('awesome'),
     ));
 }
