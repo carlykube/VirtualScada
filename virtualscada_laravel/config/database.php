2d0506912b255edc9766d96ae9f3cc5263880255
kolengue <kolengue@icloud.com>
2015-03-09 15:38:12 -0500

    put in mysql support

--- virtualscada_laravel/config/database.php
+++ virtualscada_laravel/config/database.php
@@ -54,10 +54,10 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'forge'),
-			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'host'      => 'localhost',
+			'database'  => 'vs_dev',
+			'username'  => 'vs',
+			'password'  => 'adminvs',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
