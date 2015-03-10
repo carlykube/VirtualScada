78bc3104dd9ac12a2a4d0863f6a72ef461d8eba8
poshprogrammer <poshprogrammer@gmail.com>
2015-03-10 11:22:41 -0500

    Updated MVC for Login Page

--- virtualscada_laravel/resources/views/app.blade.php
+++ virtualscada_laravel/resources/views/app.blade.php
@@ -8,6 +8,13 @@
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
+	<!-- MY CUSTOM STYLE SHEETS -->
+
+	<!--<link href="{{ asset('/css/login.css') }}" rel="stylesheet">-->
+	<link href="{{ asset('/css/custom.css') }}" rel="stylesheet">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+
 	<!-- Fonts -->
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
 
@@ -16,7 +23,10 @@
 	<!--[if lt IE 9]>
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+
 	<![endif]-->
+
+
 </head>
 <body>
 	<nav class="navbar navbar-default">
