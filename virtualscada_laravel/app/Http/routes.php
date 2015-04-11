281043ce595d161d83a815c04d671bed866bb8bf
Carly Kubacak <car.mar.kuba@gmail.com>
2015-04-11 16:51:24 -0500

    Adding ModuleController and ability to delete module from project

--- virtualscada_laravel/app/Http/routes.php
+++ virtualscada_laravel/app/Http/routes.php
@@ -11,11 +11,18 @@
 |
 */
 
+use App\Module;
 use App\Project;
 
 Route::bind('projects', function($id)
 {
 	return Project::whereId($id)->first();
+
+});
+
+Route::bind('modules', function($id)
+{
+    return Module::find($id);
 
 });
 
@@ -25,6 +32,7 @@ Route::get('home', 'ProjectController@index');
 
 // type php artisan route:list to see list of automated list of routes
 Route::resource('projects', 'ProjectController');
+Route::resource('modules', 'ModuleController');
 
 Route::get('projects/open/{projects}', 'ProjectController@open');
 Route::post('projects/addModule', 'ProjectController@addModule');
--- virtualscada_laravel/resources/views/projects/open.blade.php
+++ virtualscada_laravel/resources/views/projects/open.blade.php
@@ -38,6 +38,12 @@
                         <span class="module">
                             <h4>{{ $module->name }}</h4>
                             <p>{{ $module->file_loc }}</p>
+                            {!! Form::open(['url' => '/modules/' . $module->id, 'method'=>'DELETE']) !!}
+                            <div class="form-group">
+                                {!! Form::submit('DELETE', ['class'=>'btn btn-primary btn-md']) !!}
+                            </div>
+                            {!! Form::close() !!}
+{{--                            <a href="{{ action('ModuleController@destroy', array('id' => $module->id)) }}"> DELETE </a>--}}
                         </span>
                     @endforeach
                 </div>
