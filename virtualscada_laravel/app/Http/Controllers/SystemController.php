391fa6b1c0eae7087e593b15d4ee893a0b0c9043
Carly Kubacak <car.mar.kuba@gmail.com>
2015-04-15 00:43:07 -0500

    Sending email to all users when admin schedules downtime

--- virtualscada_laravel/app/Http/Controllers/SystemController.php
+++ virtualscada_laravel/app/Http/Controllers/SystemController.php
@@ -4,7 +4,10 @@ use App\Downtime;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use App\User;
 use Auth;
+use DB;
+use Mail;
 use Request;
 
 class SystemController extends Controller {
@@ -19,6 +22,14 @@ class SystemController extends Controller {
 
         // store downtime in database
         Downtime::create($input);
+        $i = compact('input');
+
+        // email users about downtime
+        $emails = DB::table('users')->lists('email');
+        Mail::send('emails.downtime', ['start' => $input['start_time'], 'end' => $input['end_time']], function($message) use ($emails)
+        {
+            $message->to($emails)->subject('Server Maintenance');
+        });
 
         // redirect user to home page
         flash()->success("Downtime scheduled.");
